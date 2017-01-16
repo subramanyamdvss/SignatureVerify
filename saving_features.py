89da19e5ba609d36bcd83630ed7d0cf97fc0c1d4
Sai Surya <subramanyamdvss@gmail.com>
2017-01-16 09:49:03 +0530

    prototype of saving_features for testing

--- saving_features.py
+++ saving_features.py
@@ -7,7 +7,9 @@ from inception_resnet_v2 import *
 import numpy as np
 from os import listdir
 from os.path import isfile, join
-
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import scipy.misc
 # path to your inception checkpoint file
 checkpoint_file = 'home/surya/Documents/inception_resnet_v2_2016_08_30.ckpt'
 # path to your dataset
@@ -18,24 +20,24 @@ dataset_dir_new="/home/surya/Documents/signature_features"
 create_dirs(38,dataset_dir_new)
 
 
-
-# sample_images = ['dog.jpg', 'panda.jpg']
+def creating_features:
+    sample_images = ['dog.jpg', 'panda.jpg']
 #Load the model
-# sess = tf.Session()
-# arg_scope = inception_resnet_v2_arg_scope()
-# with slim.arg_scope(arg_scope):
-#   logits, end_points = inception_resnet_v2(input_tensor, is_training=False)
-# saver = tf.train.Saver()
-# saver.restore(sess, checkpoint_file)
-# for image in sample_images:
-#   im = Image.open(image).resize((299,299))
-#   im = np.array(im)
-#   im = im.reshape(-1,299,299,3)
-#   predict_values, logit_values = sess.run([end_points['Predictions'], logits], feed_dict={input_tensor: im})
-#   print (np.max(predict_values), np.max(logit_values))
-#   print (np.argmax(predict_values), np.argmax(logit_values))
+    sess = tf.Session()
+    arg_scope = inception_resnet_v2_arg_scope()
+    with slim.arg_scope(arg_scope):
+        logits, end_points = inception_resnet_v2(input_tensor, is_training=False)
+    saver = tf.train.Saver()
+    saver.restore(sess, checkpoint_file)
+    for image in sample_images:
+        im = Image.open(image).resize((299,299))
+        im = np.array(im)
+        im = im.reshape(-1,299,299,3)
+        predict_values, logit_values = sess.run([end_points['Predictions'], logits], feed_dict={input_tensor: im})
+        print (np.max(predict_values), np.max(logit_values))
+        print (np.argmax(predict_values), np.argmax(logit_values))
 
-def creating_features(flag = True):
+def merge_images(flag = True):
     for i in range(1,39):
 
         for f1 in listdir(os.path.join(dataset_dir,"id_%d/genuine" %(i))):
@@ -51,16 +53,17 @@ def creating_features(flag = True):
                 im2 = cv2.imread(f2p)
                 im1gr = cv2.resize(cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY),(299,299))
                 im2gr = cv2.resize(cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY),(299,299))
-                if i==1:
-                    tmp=np.dstack((im1gr,im2gr))
-                    if  flag:
-                        cv2.imshow("dvss",im1gr)
-                        flag=False
-                        print tmp.shape
+        
+                if  flag:
+                    tmp=np.dstack((im1gr,im2gr,np.zeros((299,299))))
+                    flag=False
+                    print tmp.shape
+                    print tmp
+                    print scipy.misc.toimage(tmp)
                     
 
 
-creating_features()
+merge_images()
 
 
 
